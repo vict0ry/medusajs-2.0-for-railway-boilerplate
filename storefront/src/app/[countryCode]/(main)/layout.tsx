1bc5870fe4223402a9886791c7685e96a7afb55f
usamaadev <usamaadev@gmail.com>
2025-06-03 11:51:10 +0500

    changes

--- storefront/src/app/[countryCode]/(main)/layout.tsx
+++ storefront/src/app/[countryCode]/(main)/layout.tsx
@@ -12,7 +12,9 @@ export default async function PageLayout(props: { children: React.ReactNode }) {
   return (
     <>
       <Nav />
+
       {props.children}
+      
       <Footer />
     </>
   )
