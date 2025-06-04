e80f7cb4d427605967078bac1ed42ba8d2d30ce0
usamaadev <usamaadev@gmail.com>
2025-06-04 19:00:18 +0500

    Bug Fix

--- storefront/src/modules/blog/components/index.tsx
+++ storefront/src/modules/blog/components/index.tsx
@@ -41,7 +41,7 @@ const BlogsPage =  async () => {
                 </div>
                 <div className="group relative">
                   <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
-                    <a href={`/${blog.Slug}`}>
+                    <a href={`/blog/${blog.Slug}`}>
                       <span className="absolute inset-0" />
                       {blog.Title}
                     </a>
