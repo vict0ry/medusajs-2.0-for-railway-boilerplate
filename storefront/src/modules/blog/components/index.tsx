9a4755b6e8c08b6c7e3604ea034885023727717c
usamaadev <usamaadev@gmail.com>
2025-06-05 16:04:28 +0500

    [Fix] Update blog section heading and description for clarity

--- storefront/src/modules/blog/components/index.tsx
+++ storefront/src/modules/blog/components/index.tsx
@@ -9,10 +9,12 @@ const BlogsPage =  async () => {
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
-          <h2 className="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+          <h2 className="text-balance text-3xl font-semibold tracking-tight text-gray-900 sm:text-4xl">
             From the blog
           </h2>
-          <p className="mt-2 text-lg/8 text-gray-600">Learn how to grow your business with our expert advice.</p>
+            <p className="mt-2 text-lg/8 text-gray-600">
+            Discover the latest insights, benefits, and research on CBD products to enhance your wellness journey.
+            </p>
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {blogs.map((blog:any) => (
